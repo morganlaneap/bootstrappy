bcfe1d7727831d48446a883ee64efe6f23e2ce26
Morgan Lane <morgan.lane@outlook.com>
2017-09-04 20:20:56 +0100

    VERY basic setup

--- js/bootstrappy.js
+++ js/bootstrappy.js
@@ -1,3 +1,64 @@
 /**
  * Created by Morgan Lane on 04/09/2017.
  */
+
+var element = "";
+
+function bootstrappify() {
+    var editor = $('.bootstrappy-editor');
+    var result = $('.bootstrappy-result');
+    var markup = "";
+    var bootstrappy = editor.val();
+    var lines = bootstrappy.split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+
+        // this is a bootstrappy class line
+        if (line.includes('@')) {
+            line = line.replace(/\s/g,'');
+
+            var endTag = checkEndTag(line);
+
+            if (endTag != "") {
+                markup += endTag;
+                continue;
+            }
+
+            // we have a line to process
+            // so split it into its sections
+            var sections = line.replace("@", "");
+            sections = sections.split(',');
+
+            var classNames = "";
+
+            for (var section = 0; section < sections.length; section++) {
+                classNames += processSection(sections[section]);
+            }
+
+            classNames = classNames.substr(0, classNames.length - 1);
+
+            markup += "<" + element + " class=\"" + classNames + "\">";
+        }
+    }
+
+    result.text(markup);
+}
+
+function processSection(section) {
+    if (section.includes("->")) {
+        // not implemented yet
+    } else {
+        element = "div";
+        return section + " ";
+    }
+}
+
+function checkEndTag(line) {
+    switch (line) {
+        case "@end":
+            return "</div>";
+        default:
+            return "";
+    }
+}
